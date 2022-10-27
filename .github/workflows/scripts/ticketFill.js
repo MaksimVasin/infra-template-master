7b6a431cbc57c71b4b6fd7cba51da695bdcdf5b3
MaksimVasin <maksim.vasin.2001@mail.ru>
2022-10-27 22:59:43 +0300

    Ф

--- .github/workflows/scripts/ticketFill.js
+++ .github/workflows/scripts/ticketFill.js
@@ -42,7 +42,7 @@ const ticketFill = async () => {
     },
     body: JSON.stringify({
       summary: `Релиз ${ TAG } - ${ new Date().toLocaleDateString() }`,
-      description: `ветсвтенный за релиз ${ ACTOR }\n${ newComits }`
+      description: `Ответсвтенный за релиз ${ ACTOR }\n${ newComits }`
     })
   });
 }

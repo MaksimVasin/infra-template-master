2832dd416991f2c47e65a62564e51f7c25203a83
MaksimVasin <maksim.vasin.2001@mail.ru>
2022-10-27 16:59:40 +0300

    ыааы

--- .github/workflows/scripts/ticketFill.js
+++ .github/workflows/scripts/ticketFill.js
@@ -13,8 +13,10 @@ const getComits = async () => {
   };
   await exec.exec('git tag','', optionsTags);
   // отфильтровать
+  console.log('до фильтра: ', tagsArr);
   tagsArr = tagsArr.filter(item => (/^rc-\d{1,}.\d{1,}.\d{1,}$/).test(item));
-  let tags = tagsArr.length == 1? tagsArr[0] : `${ tagsArr[tagsArr.length-2] }...${tagsArr.length-1}`
+  console.log('после фильтра: ', tagsArr);
+  let tags = tagsArr.length == 1? tagsArr[0] : `${ tagsArr[tagsArr.length - 2] }...${tagsArr.length - 1}`
 
   console.log('length', tagsArr.length);
   console.log('length - 1', tagsArr.length - 1);

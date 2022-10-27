32f2c73279512ee559142776ef8dc795e5b9d77d
MaksimVasin <maksim.vasin.2001@mail.ru>
2022-10-27 03:30:08 +0300

    ssdsd

--- .github/workflows/scripts/ticketFill.js
+++ .github/workflows/scripts/ticketFill.js
@@ -2,8 +2,13 @@ import fetch from "node-fetch";
 
 const {TOKEN, ORGID, ACTOR, TAG} = process.env;
 
+const getComits = async () => {
+  return `<хеш коммита1> <автор коммита> <описание коммита>
+  <хеш коммита2> <автор коммита> <описание коммита>`
+}
+
 const ticketFill = async () => {
-  console.log('test');
+  const newComits = await getComits();
   fetch('https://api.tracker.yandex.net/v2/issues/HOMEWORKSHRI-143', {
     method: 'PATCH',
     headers: {
@@ -13,14 +18,11 @@ const ticketFill = async () => {
     },
     body: `{
       "summary": "Релиз ${ TAG } - ${ new Date().toLocaleDateString() }", 
-      "description": "Ответсвтенный за рерлиз: ${ ACTOR }"
+      "description": "ответственный за релиз ${ ACTOR }
+      коммиты, попавшие в релиз:
+      ${ newComits }"
     }`
   });
 }
 
 ticketFill();
-
-/* url: https://api.tracker.yandex.net/v2/issues/HOMEWORKSHRI-143
-      #    method: PATCH
-      #    payload: '{"summary": "Кросс-проверка Инфраструктура — Васин Максим", "description": "Описание из экшанов с секретом"}'
-      #    headers: '{"Authorization": "OAuth ${{ env.TOKEN }}", "X-Org-ID": "${{ env.ORGID }}"}' */

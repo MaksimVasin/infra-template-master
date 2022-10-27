bbe0d19a283dc2f08187ddeea1e783438e032023
MaksimVasin <maksim.vasin.2001@mail.ru>
2022-10-27 21:28:27 +0300

    ф

--- .github/workflows/scripts/dockerComment.js
+++ .github/workflows/scripts/dockerComment.js
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-const {TAG} = process.env;
+const {TOKEN, ORGID, TAG} = process.env;
 
 const ticketFill = async () => {
   fetch('https://api.tracker.yandex.net/v2/issues/HOMEWORKSHRI-143/comments', {
